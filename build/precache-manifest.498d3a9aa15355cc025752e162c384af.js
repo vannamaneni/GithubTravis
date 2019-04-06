3c2b2faf85c03f89925b6fbccf2e1d3c37fd58eb
Venkat <vr_a@outlook.com>
2019-04-06 15:07:07 +0530

    created react project

---
+++ build/precache-manifest.498d3a9aa15355cc025752e162c384af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b802231b782f9c274080",
+    "url": "/static/css/main.9dc1eb56.chunk.css"
+  },
+  {
+    "revision": "b802231b782f9c274080",
+    "url": "/static/js/main.caa64094.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "53ced46a522f76a22427134445c09b1f",
+    "url": "/index.html"
+  }
+];
